721c40bee695c796b8b0be23e3cbcf986db4d289
Manuel Schaich <rainer.schaich@eng.ox.ac.uk>
2016-04-18 17:36:54 +0100

    Added wrapper function for direct LRS call, now with file.

---
+++ LRS.m
@@ -0,0 +1,29 @@
+function [Data,data] = LRS(s)
+% [Data,data] = LRS(s)
+% Direct LRS call.
+% Structure s is used to pass data to the LRS engine.
+%
+% For V-representation:
+% s.rep = 'V'
+% s.V = V Matrix holding all vertices, each row is one vertex.
+% s.R = R Matrix holding all rays, each row is one ray.
+% At least one of the two must be non-trivial.
+% Result is H-representation {x:Data*x<=data}.
+%
+% For H-representation:
+% s.rep = 'H'
+% s.Aineq and s.bineq represent {x:s.Aineq*x<=s.bineq}.
+% s.Aeq and s.beq represent {x:s.Aeq*x==beq}.
+% The inequalities must not be omitted, the equalities are optional.
+% Result is V-representation Data(i,:) is vertex if data(i)==1, ray
+% for data(i) == 0.
+% 
+% Additional parameters are:
+% s.maxdepth, specifies trancation of treesearch.
+% s.maxoutput, truncates maximal number of output.
+% s.maxcobases, limits the number of explored cobases.
+
+
+temp = calllib('libgeocalc','lrsCall',s);
+Data = temp{1};
+data = temp{2};
